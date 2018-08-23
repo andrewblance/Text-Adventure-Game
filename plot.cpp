36e0edc69f03cf5e99e1546c746a4fca1b14c74a
Andrew Blance <andrewblance@Andrews-MacBook.local>
2018-08-23 16:59:41 +0100

    now RtFR

--- plot.cpp
+++ plot.cpp
@@ -4,14 +4,14 @@ using namespace std;
 vector<Menu> game{
     Menu("start",    //Level Name
 
-        "Would you like to go to the First Level or the Second Level, YourName?",   //Text that will be displayed
+        "oh, your name is YourName, I guess thats a pretty neat name! \n\nnow that we're friends YourName, I feel comfortable enough to tell you i'm thinking of maybe kinda taking over the world today. \n\nI have lots of fun plans for it! Would you like to help me?",   //Text that will be displayed
 
-        "That was not an option",       //What will be displayed if you type something that isnt an option
+        "Aw darn I totally don't want to misinterpret what you're saying cause this is like pretty important, so if you can just choose from:",       //What will be displayed if you type something that isnt an option
 
         vector<tuple <string, string,string> >{
-        {"first_level", "1", "Empty"}, {"second_level", "2", "shoes"}},   //Options that can be chosen, and the level you will be sent to
+        {"yes", "Agree", "Empty"}, {"no", "Disagree", "Empty"}},   //Options that can be chosen, and the level you will be sent to
         vector<string>
-        {"poop", "shoes"}),
+        {}),
 
 
     Menu("1",
